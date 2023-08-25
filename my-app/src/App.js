eb4351b4302b438aecb1d940d2b1b8f8c57504b4
kaushal <kmp120901@gmail.com>
2023-08-25 16:27:01 +0530

    addes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,6 @@ import Alert from './components/Alert';
 function App() {
   return (
     <>
-   <Navbar title="TextUtils2" aboutText="about textUtils"/>
    <Alert alert="this is alert"/>
    <div className="container">
    <TextForm heading="Enter the text to analyse"/>
